e3441a6e1d3b1d10f2ba5486d36bfb125f4b66ef
Ali <ali.ghadirzadeh@gmail.com>
2021-10-18 14:17:17 +0200

    pnn bug fixed

--- mbrl/third_party/pnn/pnn.py
+++ mbrl/third_party/pnn/pnn.py
@@ -7,6 +7,7 @@ class PNNColumn(nn.Module):
     def __init__(self, n_tasks, input_size, hidden_size, output_size, device):
         super(PNNColumn, self).__init__()
         print('device is ', device)
+        print('current task id is ', n_tasks)
         self.device = device
         self.n_tasks = n_tasks
         self.sizes = [hidden_size, output_size]
@@ -30,22 +31,33 @@ class PNNColumn(nn.Module):
             self.alpha[i].append(nn.Parameter(torch.tensor(1e-2)))
             self.alpha[i].append(nn.Parameter(torch.tensor(1e-2)))
         self._reset_parameters()
+        self.w = self.w.to(self.device)
+        self.u = self.u.to(self.device)
+        self.v = self.v.to(self.device)
+        self.alpha = self.alpha.to(self.device)
 
     def forward(self, x, outputs):
         batch_size = x.shape[0]
         if self.n_tasks == 0:
-            outputs = torch.zeros(1, self.n_layers-1, batch_size, self.hidden_size)
+            outputs = torch.zeros(1, self.n_layers-1, batch_size, self.hidden_size).to(self.device)
         else:
-            outputs = torch.cat((outputs, torch.zeros(1, self.n_layers-1, batch_size, self.hidden_size)))
-        y = self.w[0](x)
+            outputs = torch.cat((outputs, torch.zeros(1, self.n_layers-1,
+                                                      batch_size, self.hidden_size))).to(self.device)
+        y = F.relu(self.w[0](x))
         outputs[self.n_tasks][0] = y.detach()
         for layer in range(self.n_layers-1):
-            u_out = torch.zeros(batch_size, self.sizes[layer], device=self.device)
             for k in range(self.n_tasks):
                 v_out = self.v[k][layer](self.alpha[k][layer] * (outputs[k][layer]))
-                u_out += self.u[k][layer](F.relu(v_out))
-            y = F.relu(u_out + self.w[layer+1](y))
+                if k == 0:
+                    u_out = self.u[k][layer](F.relu(v_out))
+                else:
+                    u_out += self.u[k][layer](F.relu(v_out))
+            if self.n_tasks == 0:
+                y = self.w[layer + 1](y)
+            else:
+                y = u_out + self.w[layer+1](y)
             if layer != (self.n_layers-2):
+                y = F.relu(y)
                 outputs[self.n_tasks][layer + 1] = y.detach()
         return y, outputs
 
